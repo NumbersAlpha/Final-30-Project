ab062e7a19ccffdc755153a0b95276aea1e961be
Teknoses <99210035+Teknoses@users.noreply.github.com>
2023-06-13 11:36:22 -0400

    Adds a 'p' if it is a pawn move

--- Translator/script.js
+++ Translator/script.js
@@ -1,3 +1,33 @@
-const string = 'e4 e5 nf3 nc6'
-let words = string.split('')
-document.write(words)
+const string = 'e4 e5 exd6 nf3 nc6'
+let pawns = []
+let split = string.split(' ')
+split.forEach(element => pawn(element))
+console.log(pawns)
+//words.forEach(element => translation(element))
+//console.log(words)
+
+function pawn(element){
+  if(element.length == 2){
+    pawns.push(`p${element}`)
+  }
+  else if(element.length == 4){
+    if(element.match(/[a-h]/)){
+       pawns.push(`p${element}`)
+    }
+    else{
+      pawns.push(`${element}`)
+    }
+  }
+  else{
+    pawns.push(element)
+  }
+}
+function translation(element){
+  if(element.match(/^[0-9]$/i)){
+
+  }
+  else if(element.match(/^[a-h]$/i)){
+   
+  }
+  
+}
